605bcb926e4498fe643dc9250a696ad2aca36288
Gowtham T <kuttygowtham756@gmail.com>
2025-10-14 07:36:45 +0530

    Costing method Changes

---
+++ FinanceApi/ModelDTO/CostingMethodDTO.cs
@@ -0,0 +1,15 @@
+﻿namespace FinanceApi.ModelDTO
+{
+    public class CostingMethodDTO
+    {
+        public long ID { get; set; }
+        public string CostingName { get; set; }
+        public string description { get; set; }
+        public long CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+
+        public long UpdatedBy { get; set; }
+        public DateTime UpdatedDate { get; set; }
+        public bool IsActive { get; set; } = true;
+    }
+}
