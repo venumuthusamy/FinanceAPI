e325756222a59d3d466e0a20effb2a9af1d22111
AnusuyaMuthu <anu014634@gmail.com>
2025-11-09 22:04:50 -0800

    DeliveryOrder changes

--- FinanceApi/Interfaces/IDeliveryOrderRepository.cs
+++ FinanceApi/Interfaces/IDeliveryOrderRepository.cs
@@ -1,4 +1,4 @@
-﻿// File: Interfaces/IDeliveryOrderRepository.cs
+﻿using FinanceApi.ModelDTO;
 using static FinanceApi.ModelDTO.DeliveryOrderDtos;
 
 namespace FinanceApi.Interfaces
@@ -6,16 +6,17 @@ namespace FinanceApi.Interfaces
     public interface IDeliveryOrderRepository
     {
         Task<int> CreateAsync(DoCreateRequest req, int userId);
+        Task<IEnumerable<DoHeaderDto>> GetAllAsync();
         Task<DoHeaderDto?> GetHeaderAsync(int id);
         Task<IEnumerable<DoLineDto>> GetLinesAsync(int doId);
-        Task<IEnumerable<DoHeaderDto>> GetAllAsync();
-
         Task UpdateHeaderAsync(int id, DoUpdateHeaderRequest req, int userId);
+
         Task<int> AddLineAsync(DoAddLineRequest req, int userId);
         Task RemoveLineAsync(int lineId);
 
         Task SetStatusAsync(int id, int status, int userId);
         Task PostAsync(int id, int userId);
+
         Task<IEnumerable<object>> GetSoRedeliveryViewAsync(int doId, int soId);
     }
 }
