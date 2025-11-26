18f7997035481e0066988e65835ff17286b937bf
Gowtham T <kuttygowtham756@gmail.com>
2025-11-26 10:09:44 +0530

    changes

--- FinanceApi/ModelDTO/ManualJournalCreateDto.cs
+++ FinanceApi/ModelDTO/ManualJournalCreateDto.cs
@@ -13,6 +13,7 @@ namespace FinanceApi.ModelDTO
         public string? Description { get; set; }
         public decimal Debit { get; set; }
         public decimal Credit { get; set; }
+        public int? ItemId { get; set; }
 
         // Recurring flags
         public bool IsRecurring { get; set; }
