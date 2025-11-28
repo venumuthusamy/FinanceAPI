7742148925bd53b32e1850967499917cc94cea8a
venumuthusamy <venumuthusamy@gmail.com>
2025-11-28 07:48:50 +0530

    trial balance changes

--- FinanceApi/ModelDTO/TrialBalanceDTO.cs
+++ FinanceApi/ModelDTO/TrialBalanceDTO.cs
@@ -4,9 +4,14 @@ namespace FinanceApi.ModelDTO.TB
     public class TrialBalanceDTO
     {
         public int HeadId { get; set; }
-        public string HeadCode { get; set; }
-        public string HeadName { get; set; }
-        public decimal Debit { get; set; }
-        public decimal Credit { get; set; }
+        public string HeadCode { get; set; } = string.Empty;
+        public string HeadName { get; set; } = string.Empty;
+        public int? ParentHead { get; set; }
+
+        public decimal OpeningDebit { get; set; }
+        public decimal OpeningCredit { get; set; }
+
+        public decimal ClosingDebit { get; set; }
+        public decimal ClosingCredit { get; set; }
     }
 }
