eb2b78ae29fc73f73dfadc00a4c5c32510bac4d9
Gowtham T <kuttygowtham756@gmail.com>
2026-01-20 14:34:48 +0530

    material request changes

---
+++ FinanceApi/InterfaceService/IMaterialRequisitionService.cs
@@ -0,0 +1,14 @@
+﻿using FinanceApi.ModelDTO;
+using FinanceApi.Models;
+
+namespace FinanceApi.InterfaceService
+{
+    public interface IMaterialRequisitionService
+    {
+        Task<IEnumerable<MaterialRequisitionDTO>> GetAllAsync();
+        Task<MaterialRequisitionDTO> GetById(int id);
+        Task<int> CreateAsync(MaterialRequisition mrq);
+        Task UpdateAsync(MaterialRequisition mrq);
+        Task DeleteAsync(int id);
+    }
+}
---
+++ FinanceApi/Models/MaterialRequisitionLine.cs
@@ -0,0 +1,21 @@
+﻿namespace FinanceApi.Models
+{
+    public class MaterialRequisitionLine
+    {
+        public int Id { get; set; }
+        public int MaterialReqId { get; set; }
+        public int ItemId { get; set; }
+
+        public string? ItemCode { get; set; }
+
+        public string? ItemName { get; set; }
+
+        public int? UomId { get; set; }
+
+        public string? UomName { get; set; }
+
+        public decimal Qty { get; set; }
+
+        public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
+    }
+}
